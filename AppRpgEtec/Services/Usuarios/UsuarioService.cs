6df7adcdd6b12a98f41b150065552f6654e2af6d
Lucas Felix <Lucas.felixandrade@hotmail.com.br>
2024-03-13 22:43:43 -0300

    feat(usuario): add service layer in mvvm architecture

---
+++ AppRpgEtec/Services/Usuarios/UsuarioService.cs
@@ -0,0 +1,33 @@
+﻿using AppRpgEtec.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppRpgEtec.Services.Usuarios
+{
+    public class UsuarioService : Request
+    {
+        private readonly Request _request;
+        private const string apiUrlBase = "http://xyz.somee.com/RpgApi/Usuarios";
+
+        public UsuarioService()
+        {
+            _request = new Request();
+        }
+        public async Task<Usuario> PostRegistrarUsuarioAsync(Usuario usuario)
+        {
+            string uriComplementar = "/Registrar";
+            usuario.Id = await _request.PostReturnIntAsync(apiUrlBase + uriComplementar, usuario);
+            return usuario;
+        }
+
+        public async Task<Usuario> PostAutenticarUsuarioAsync(Usuario usuario)
+        {
+            string uriComplementar = "/Autenticar";
+            usuario = await _request.PostAsync(apiUrlBase + uriComplementar, usuario, string.Empty);
+            return usuario;
+        }
+    }
+}
---
+++ AppRpgEtec/ViewModels/Usuarios/UsuarioViewModel.cs
@@ -0,0 +1,50 @@
+﻿using AppRpgEtec.Services.Usuarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AppRpgEtec.ViewModels.Usuarios
+{
+    public class UsuarioViewModel : BaseViewModel
+    {
+        private UsuarioService _uService;
+
+        #region Commands
+        public ICommand RegistrarCommand { get; set; }
+        public ICommand AutenticarCommand { get; set; }
+        #endregion
+
+        #region AtributosPropiedades
+        private string login = string.Empty;
+        private string password = string.Empty;
+
+        public string Login 
+        { 
+            get => login; 
+            set
+            {
+                login = value;
+                OnPropertyChanged(Login);
+            }
+                 
+        }
+        
+        public string Password
+        {
+            get => password;
+            set 
+            { 
+                password = value;
+                OnPropertyChanged(Password);
+            }
+        }
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
